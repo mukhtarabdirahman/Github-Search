da331ff58ad98ec3b6b297d8cb81dd336292306b
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-09 08:28:12 +0300

    date pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    DateCountPipe
     
   ],
   imports: [
